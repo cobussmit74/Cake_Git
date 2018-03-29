a21f1395a7d41b4c56e127a7a3bde8c9f106cb5e
Cobus Smit <cobus.smit@chillisoft.co.za>
2018-03-29 13:51:58 +0200

    Added alias for Submodule Update command

---
+++ src/Cake.Git/GitAliases.Submodule.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+using Cake.Git.Extensions;
+using LogLevel = Cake.Core.Diagnostics.LogLevel;
+using Verbosity = Cake.Core.Diagnostics.Verbosity;
+
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+namespace Cake.Git
+{
+    // ReSharper disable once PublicMembersMustHaveComments
+    public static partial class GitAliases
+    {
+        /// <summary>
+        /// Update a specific submodule.
+        /// </summary>
+        /// <example>
+        /// <code>
+        ///     GitSubmoduleUpdate("c:/temp/cake", "icing-sugar", new GitSubmoduleUpdateSettings{ Init = true });
+        /// </code>
+        /// </example>
+        /// <param name="context">The context.</param>
+        /// <param name="repositoryDirectoryPath">Path to repository.</param>
+        /// <param name="submoduleName">Name of the submodule.</param>
+        /// <param name="updateSettings">The update settings.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        [CakeMethodAlias]
+        [CakeAliasCategory("SubmoduleUpdate")]
+        public static void GitSubmoduleUpdate(
+            this ICakeContext context,
+            DirectoryPath repositoryDirectoryPath,
+            string submoduleName,
+            GitSubmoduleUpdateSettings updateSettings
+            )
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (repositoryDirectoryPath == null)
+            {
+                throw new ArgumentNullException(nameof(repositoryDirectoryPath));
+            }
+
+            if (string.IsNullOrWhiteSpace(submoduleName))
+            {
+                throw new ArgumentNullException(nameof(submoduleName));
+            }
+
+            if (updateSettings == null)
+            {
+                throw new ArgumentNullException(nameof(updateSettings));
+            }
+            
+            context.UseRepository(
+                repositoryDirectoryPath,
+                repository => repository.Submodules.Update(submoduleName, updateSettings.ToSubmoduleUpdateOptions())
+            );
+        }
+
+        /// <summary>
+        /// Update all submodules.
+        /// </summary>
+        /// <example>
+        /// <code>
+        ///     GitSubmoduleUpdate("c:/temp/cake", new GitSubmoduleUpdateSettings{ Init = true });
+        /// </code>
+        /// </example>
+        /// <param name="context">The context.</param>
+        /// <param name="repositoryDirectoryPath">Path to repository.</param>
+        /// <param name="updateSettings">The update settings.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        [CakeMethodAlias]
+        [CakeAliasCategory("SubmoduleUpdate")]
+        public static void GitSubmoduleUpdate(
+            this ICakeContext context,
+            DirectoryPath repositoryDirectoryPath,
+            GitSubmoduleUpdateSettings updateSettings
+        )
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (repositoryDirectoryPath == null)
+            {
+                throw new ArgumentNullException(nameof(repositoryDirectoryPath));
+            }
+            
+            var submoduleNames = context.UseRepository(
+                repositoryDirectoryPath,
+                repository =>
+                {
+                    return repository
+                        .Submodules
+                        .Select(s => s.Name)
+                        .ToList();
+                });
+
+            foreach (var name in submoduleNames)
+            {
+                GitSubmoduleUpdate(context, repositoryDirectoryPath, name, updateSettings);
+            }
+        }
+    }
+}
---
+++ src/Cake.Git/GitSubmoduleUpdateSettings.cs
@@ -0,0 +1,23 @@
+﻿using LibGit2Sharp;
+
+namespace Cake.Git
+{
+    /// <summary>
+    /// Contains settings used by GitSubmoduleUpdate.
+    /// </summary>
+    public class GitSubmoduleUpdateSettings
+    {
+        /// <summary>
+        /// True will initialize the local configuration file.
+        /// </summary>
+        public bool Init { get; set; }
+
+        internal SubmoduleUpdateOptions ToSubmoduleUpdateOptions()
+        {
+            return new SubmoduleUpdateOptions
+            {
+                Init = this.Init
+            };
+        }
+    }
+}
